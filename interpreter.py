90337c7a7f3c246c297a61dbd95744b5258808a5
Zack Brandon <goldstellar34@gmail.com>
2021-04-29 17:52:24 -0400

    Math interpreter complete (base functionality)

--- interpreter.py
+++ interpreter.py
@@ -2,7 +2,48 @@
 from nodes import *
 from values import Number
 
+#The interpreter will take the base node (the 'top' node) and recursively call the .visit()
+#method on all of the corresponding nodes, ultimately returning a number.
 class Interpreter:
     #Pass in the root node of our tree. It will be processed and then return a number
     def visit(self, node):
-        pass
+        #Create a string of the method that we want to call based on the node passed to .visit()
+        #Ex. AddNode => visit_AddNode
+        method_name = f'visit_{type(node).__name__}'
+        #Method will now be the method that we need to call based on what was passed into .visit()
+        method = getattr(self, method_name) #if an addNode is passed in, then method now equals visit_AddNode()
+        #return the appropriate method called on the passed in node
+        #method(node) returns 
+        return method(node)
+    
+    def visit_NumberNode(self,node):
+        #Returns a number value of the passed in number node
+        return Number(node.value)
+    
+    #Returns the value of two nodes
+    def visit_AddNode(self, node):
+        return Number(self.visit(node.node_a).value + self.visit(node.node_b).value)
+
+    #Returns the value of two nodes
+    def visit_SubtractNode(self, node):
+        return Number(self.visit(node.node_a).value - self.visit(node.node_b).value)
+
+    #Returns the value of two nodes
+    def visit_MultiplyNode(self, node):
+        return Number(self.visit(node.node_a).value * self.visit(node.node_b).value)
+
+    #Returns the value of two nodes
+    def visit_DivideNode(self, node):
+        #Error catching (for math divide by zero)
+        try:
+            return Number(self.visit(node.node_a).value / self.visit(node.node_b).value)
+        except:
+            raise Exception("Runtime math error")
+
+    #returns the value of the node
+    def visit_PlusNode(self, node):
+        return self.visit(node.node)
+    
+    #returns a negated version of the node value
+    def visit_MinusNode(self, node):
+        return Number(-self.visit(node.node).value)
--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 from lexer import Lexer
 from parser_ import Parser
+from interpreter import Interpreter
 
 #main program
 
 while True:
-    #store the user input for each line in the 'text' variable
-    text = input("calc > ")
-    #pass that into our lexer to generate our tokens (which takes text as it's only parameter)
-    lexer = Lexer(text)
-    #Generate the tokens from the lexer, and store them in the 'tokens' variable
-    tokens = lexer.generate_tokens()
-    #pass in our generated tokens list to Parser
-    parser = Parser(tokens)
-    #Assign the result of parser.parse() to the 'tree' variable
-    tree = parser.parse()
-    print(tree)
+    try:
+        #store the user input for each line in the 'text' variable
+        text = input("calc > ")
+        #pass that into our lexer to generate our tokens (which takes text as it's only parameter)
+        lexer = Lexer(text)
+        #Generate the tokens from the lexer, and store them in the 'tokens' variable
+        tokens = lexer.generate_tokens()
+        #pass in our generated tokens list to Parser
+        parser = Parser(tokens)
+        #Assign the result of parser.parse() to the 'tree' variable
+        tree = parser.parse()
+        #If there is no tree, skip the following lines
+        if not tree: continue
+        #create a new interpreter, assign the 'interpreter' variable to it.
+        interpreter = Interpreter()
+        #Interpreter takes a tree (of nodes) and returns a value from it.
+        value = interpreter.visit(tree)
+        print(value)
+    #If there is a problem anywhere in here, print the exception
+    except Exception as e:
+        print(e)
