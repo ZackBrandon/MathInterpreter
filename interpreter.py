1b8fd9a07e0c285585286e2e93c08a4d62a3f95d
Zack Brandon <goldstellar34@gmail.com>
2021-04-29 15:38:39 -0400

    Added code

--- interpreter.py
+++ interpreter.py
@@ -5,4 +5,4 @@ from values import Number
 class Interpreter:
     #Pass in the root node of our tree. It will be processed and then return a number
     def visit(self, node):
-        
+        pass
